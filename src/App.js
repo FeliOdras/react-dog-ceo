1c8ac95cb3e42cdcc35d0da466a31f07f3e91824
FeliOdras <ariane.feldmann@odras.de>
2019-06-05 13:57:23 +0200

    Import connect, refacor app to class

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+import { connect } from 'react-redux';
+
+class App extends component {
+  return(
+    <div className = "App" >
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
