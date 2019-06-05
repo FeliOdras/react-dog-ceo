3e21091f0a4485a16368e80673f0df42dd1243a4
FeliOdras <ariane.feldmann@odras.de>
2019-06-05 10:55:19 +0200

    Created reducer function

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,3 +5,16 @@ const initialState = {
     dog: null,
     error: null
 };
+
+export function reducer(state = initialState, action) {
+    switch (action.type) {
+        case API_CALL_REQUEST:
+            return { ...state, fetching: true, error: null };
+        case API_CALL_SUCCESS:
+            return { ...state, fetching: false, dog: action.dog };
+        case API_CALL_FAILURE:
+            return { ...state, fetching: false, dog: null, error: action.error };
+        default:
+            return state;
+    }
+}
