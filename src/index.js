c4b367aedb47ba13b2b68373d5b8989ea640f209
FeliOdras <ariane.feldmann@odras.de>
2019-06-05 13:09:49 +0200

    Updated imports on entry point

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import { Provider } from 'react-redux';
+import store from './store';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
