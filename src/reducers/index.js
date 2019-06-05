f3971da0c0214780e44b906ce111ae32aaca8fed
FeliOdras <ariane.feldmann@odras.de>
2019-06-05 10:54:51 +0200

    Added initial state to reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,7 @@
-import { API_CALL_REQUEST, API_CALL_SUCCESS, API_CALL_FAILURE } from '../actions/actionTypes'
+import { API_CALL_REQUEST, API_CALL_SUCCESS, API_CALL_FAILURE } from '../actions/actionTypes';
+
+const initialState = {
+    fetching: false,
+    dog: null,
+    error: null
+};
