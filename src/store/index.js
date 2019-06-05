63cb0b058f42f45aafc0f741229dce5a004e603d
FeliOdras <ariane.feldmann@odras.de>
2019-06-05 13:05:00 +0200

    Added DvTools

--- src/store/index.js
+++ src/store/index.js
@@ -5,3 +5,6 @@ import { reducer } from '../reducers';
 
 const sagaMiddleware = createSagaMiddleware();
 
+const reduxDevTools =
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
