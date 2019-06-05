338548485c9d686593b7a324168d6a28fb2f3cf6
FeliOdras <ariane.feldmann@odras.de>
2019-06-05 13:10:59 +0200

    Updated entry points render

--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ import store from './store';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
