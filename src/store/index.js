d5e35199177451bbb12be3057b38976636424556
FeliOdras <ariane.feldmann@odras.de>
2019-06-05 13:06:51 +0200

    Added stor variable

--- src/store/index.js
+++ src/store/index.js
@@ -8,3 +8,8 @@ const sagaMiddleware = createSagaMiddleware();
 const reduxDevTools =
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
+export const store = createStore(
+    reducer,
+    compose(applyMiddleware(sagaMiddleware), reduxDevTools)
+)
+
